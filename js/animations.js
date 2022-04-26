9e96c1bc88089689fca50e116e1d11c721e3ba6d
Perova Ekaterina <caterina.kape.11@inbox.ru>
2022-04-26 23:07:11 +0300

    Плавный скролл

--- js/animations.js
+++ js/animations.js
@@ -1,48 +1,88 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+let animationInited = false;
 
-function increaseNumberAnimationStep(i, element, endNumber){
-    if (i<=endNumber){
-      if(i===endNumber){
-          element.innerText=i+'+';
-      }else{
-          element.innerText=i;
-      }
-      i+=100;
+function increaseNumberAnimationStep(i, element, endNumber) {
+    if (i <= endNumber) {
+        if (i === endNumber) {
+            element.innerText = i + '+';
+        } else {
+            element.innerText = i;
+        }
+        i += 100;
 
-setTimeout(function(){
-    increaseNumberAnimationStep(i, element, endNumber);
-}, INCREASE_NUMBER_ANIMATION_SPEED);
-}
+        setTimeout(function () {
+            increaseNumberAnimationStep(i, element, endNumber);
+        }, INCREASE_NUMBER_ANIMATION_SPEED);
+    }
 }
 
 function initIncreaseNumberAnimation() {
-    
-    let element=document.querySelector(".features__clients-count");
-    
- 
-    increaseNumberAnimationStep(0,element,5000);
+
+    let element = document.querySelector(".features__clients-count");
+
+
+    increaseNumberAnimationStep(0, element, 5000);
 }
-initIncreaseNumberAnimation();
+// initIncreaseNumberAnimation();
 
 
 
 // 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
     if (event.target.value === 'other') {
-        let formContainer=document.createElement("div");
+        let formContainer = document.createElement("div");
         formContainer.classList.add("form__group", "form__other-input");
 
-        let input=document.createElement("input");
-        input.placeholder="Введите ваш вариант";
-        input.type="text";
+        let input = document.createElement("input");
+        input.placeholder = "Введите ваш вариант";
+        input.type = "text";
         formContainer.appendChild(input);
-        document.querySelector("#form form").insertBefore(formContainer,document.querySelector('.form__submit'));
+        document.querySelector("#form form").insertBefore(formContainer, document.querySelector('.form__submit'));
 
-      } else  {
-          let otherInput=document.querySelector(".form__other-input");
-          if(Boolean(otherInput)){
+    } else {
+        let otherInput = document.querySelector(".form__other-input");
+        if (Boolean(otherInput)) {
             otherInput.parentElement.removeChild(otherInput);
-          }
-      }
+        }
+    }
+});
+// 
+
+function updateScroll() {
+    let header = document.querySelector("header");
+    if (window.scrollY > 0) {
+        header.className = "header__scrolled";
+    } else {
+        if (header.className === "header__scrolled") {
+            header.className = ""
+        }
+    }
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if (windowBottomPosition >= countElementPosition && !animationInited) {
+        animationInited = true;
+        initIncreaseNumberAnimation();
+    }
+};
+
+window.addEventListener("scroll", updateScroll)
+
+//
+
+function addSmoothScroll(anchor) {
+    anchor.addEventListener('click', onLinkClick);
+      
+
+  }
+
+  function onLinkClick(event) {
+    event.preventDefault();
+   
+    document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
-    
+  }
+
+let aCollection=document.querySelectorAll('[href^="#"]');
+aCollection.forEach(element => addSmoothScroll(element));
+
